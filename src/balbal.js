d8d5151039301732dd8616a2202556738d13520e
coomdev <coomer@coom.tech>
2022-04-28 12:36:19 +0200

    preserve current b4k anti-pee obfuscate script for posterity

--- src/balbal.js
+++ src/balbal.js
@@ -1,1 +1,79 @@
-balbal.js
+function toriah() {
+    const daniil = ["W6lcMXyeW4aeqCo+WRddGW", "Eu98WOq", "FmkuDgRdG8kmhmoSWRVdVbrmW7y", "hCoppXLoW6hcUZiPW4e", "W6SsWRLuuHZdJwRcN8ok", "W6FcLCkBWQn6", "sCkyleGcWQddQgWaW7SPkXjJ", "W4Wtgmo5WRa3jSo1WQldTCkwWPFdUG", "oSkCW5FcO1RdISomW6KnWR8Laa", "WRJcJGSvWQ4ytmo4WRRdGq", "W73cHmonW7ldUYah", "WRCBWRXlf1RdH2dcJSkc", "usZdNLJdU8kktmo1ia", "W6FcIWScW7O4sSoMWRddJG", "kHmGWOKNaIldHSkv", "j8o6WPix", "ix1RtMK+WQrEWP8lW6NcOa", "W7DXxmk1ef0S", "CsmjjmkrWOxdRea", "c8oFzHbt", "W5xcLCkFWQfIWP/cKYnHWRG", "W5C6WPbFbKddIvBcL8oa", "WQzNyCk2", "jvhcSq", "W7hdUSk9uu/cVmknubtcLwJcHSkx", "W5z9W7ddTmoJWPP8fSkuia", "WQTfW6iEraddRvRcICoZEmkf", "WOZdMCkaqmoJzceu", "yWfKCtn5W4SjWOy", "W6JcGSoDW5NdTmohbmkJqSoQbmoZW60", "sJRdKG", "qCoeCCo1xbtdMCoWW5RcGH16d8of", "WO3dSNXJmCoVWRX3W54", "DmkTW4DFjJnIWRvkW5FcSa", "WOpdVgjRkG", "wmoOW7ag", "WQHfW6qwr1FdI1ZcV8oyEq", "CSo5nmo2WPK", "mSksW4NdQKxcHWJcQvmZ", "AfrHWPeh", "W4VdMMZdOW", "stRcTCk0", "WQfLymkHfG", "odVdMK0+", "W6lcLCokWPVcOCkrumkx", "e8kslmk1dvm", "rYBdGLu", "mmk+DCoUWRNdICkFW79o", "W47dO2f3lmoEWOT3W4ddHG", "cglcRmk/WRPko8kvDG", "WRTclmo9WQ/cQd5vDSozu8oKWOC", "WRhdJSodW7iOW43cHbz+WQa7rtK", "WOrVWOJcKeK", "WOldSM1Vp8oZWODTW4ddLG", "WQVcKCoXW5a", "krGVW4ruzcVdLCkGW755WRe", "mZddPCoVWRddSmkAweNcJa", "WPbgwG", "WRddJ8kmWO91WPBdOZi", "W4tdHSkluCkDFJ4fW7ldLW"];
+    toriah = function () {
+      return daniil;
+    };
+    return toriah();
+  }
+  function galit(parthiv, alexan) {
+    const stephney = toriah();
+    return galit = function (elvenia, yuriel) {
+      elvenia = elvenia - 480;
+      let theordore = stephney[elvenia];
+      if (galit.bBNAda === undefined) {
+        var celedonio = function (kywaun) {
+          const aranya = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+          let phinizy = "", nonna = "";
+          for (let fumiko = 0, shakella, mitesh, habibatou = 0; mitesh = kywaun.charAt(habibatou++); ~mitesh && (shakella = fumiko % 4 ? shakella * 64 + mitesh : mitesh, fumiko++ % 4) ? phinizy += String.fromCharCode(255 & shakella >> (-2 * fumiko & 6)) : 0) {
+            mitesh = aranya.indexOf(mitesh);
+          }
+          for (let samarie = 0, akelius = phinizy.length; samarie < akelius; samarie++) {
+            nonna += "%" + ("00" + phinizy.charCodeAt(samarie).toString(16)).slice(-2);
+          }
+          return decodeURIComponent(nonna);
+        };
+        const dereonna = function (elenora, datra) {
+          let malina = [], ramazan = 0, melodi, cressie = "";
+          elenora = celedonio(elenora);
+          let correena;
+          for (correena = 0; correena < 256; correena++) {
+            malina[correena] = correena;
+          }
+          for (correena = 0; correena < 256; correena++) {
+            ramazan = (ramazan + malina[correena] + datra.charCodeAt(correena % datra.length)) % 256, melodi = malina[correena], malina[correena] = malina[ramazan], malina[ramazan] = melodi;
+          }
+          correena = 0, ramazan = 0;
+          for (let aaryahi = 0; aaryahi < elenora.length; aaryahi++) {
+            correena = (correena + 1) % 256, ramazan = (ramazan + malina[correena]) % 256, melodi = malina[correena], malina[correena] = malina[ramazan], malina[ramazan] = melodi, cressie += String.fromCharCode(elenora.charCodeAt(aaryahi) ^ malina[(malina[correena] + malina[ramazan]) % 256]);
+          }
+          let r = cressie;
+          console.log(cressie)
+          return r;
+        };
+        galit.bwFABL = dereonna, parthiv = arguments, galit.bBNAda = !![];
+      }
+      const shatiek = stephney[0], samani = elvenia + shatiek, rahmell = parthiv[samani];
+      return !rahmell ? (galit.FpkEsr === undefined && (galit.FpkEsr = !![]), theordore = galit.bwFABL(theordore, yuriel), parthiv[samani] = theordore) : theordore = rahmell, theordore;
+    }, galit(parthiv, alexan);
+  }
+  (function (matthan, andromeda) {
+    const germane = {_0x448e06: 499, _0x27abc6: 483, _0x3b23b6: "OtER", _0x4ce8cb: 506, _0xd111ac: 491}, bonnelle = galit, drekwon = matthan();
+    while (!![]) {
+      try {
+        const markous = -parseInt(bonnelle(germane._0x448e06, "HJ[Q")) / 1 + -parseInt(bonnelle(535, "Bomr")) / 2 + parseInt(bonnelle(germane._0x27abc6, germane._0x3b23b6)) / 3 * (parseInt(bonnelle(523, "&8%y")) / 4) + parseInt(bonnelle(germane._0x4ce8cb, "q^O!")) / 5 * (parseInt(bonnelle(487, "$JI[")) / 6) + parseInt(bonnelle(481, "k)sm")) / 7 * (-parseInt(bonnelle(germane._0xd111ac, "K&JK")) / 8) + -parseInt(bonnelle(524, "n0NA")) / 9 + parseInt(bonnelle(504, "1i8E")) / 10;
+        if (markous === andromeda) break; else drekwon.push(drekwon.shift());
+      } catch (roane) {
+        drekwon.push(drekwon.shift());
+      }
+    }
+  }(toriah, 403685), function () {
+    const kylla = {_0xbdc399: 498, _0x449282: "s[zI", _0x445e76: 501, _0x417ba3: "Y4s(", _0x17d25f: 519, _0x12de39: "3@Sn", _0x3ae961: 510, _0x2a03c0: "]4F3", _0x2e0f85: 526, _0x152844: "1!(7", _0x4634b7: 538, _0x2b9f7e: "n0NA", _0x14d2e5: 488, _0x476988: 525, _0x18e965: "w6&%", _0x411781: "ue[Y", _0x16b113: 527, _0x32f03c: "Pm#r", _0x4af385: 500, _0x3cca4d: "rezn", _0x49b918: "$JI[", _0x39eee4: 485, _0x5f39fa: "3@Sn", _0x452acc: "1i8E", _0x19ccbb: 534, _0x38f6f2: "WRn(", _0x506e68: 505, _0x5daea9: 495, _0x31d246: 533, _0x4587b4: "n0NA", _0x3474bd: "o$@]", _0x389a5c: 494, _0x1e9227: "HJ[Q", _0x153808: 536, _0x1ee224: 511, _0xdeb9f0: 522}, okla = {_0x398768: 532, _0x2290c2: "rezn", _0x589a61: 484, _0x4df3e6: "HJ[Q", _0x24da8a: 517, _0x2b63fc: "TE*P", _0x40dca1: 521, _0x473840: "1!(7", _0x2fcf6b: 503, _0xb95a18: "3@Sn", _0x2fab67: 496};
+    let yalissa = ![], maichael = [function () {
+      const zekai = galit;
+      let naishaun = [zekai(okla._0x398768, okla._0x2290c2) + zekai(530, "jh1y"), zekai(482, "udQq") + "l", zekai(okla._0x589a61, okla._0x4df3e6) + zekai(okla._0x24da8a, okla._0x2b63fc), zekai(okla._0x40dca1, okla._0x473840) + zekai(537, okla._0x4df3e6) + zekai(okla._0x2fcf6b, okla._0xb95a18)];
+      for (let makinlie of naishaun) {
+        if (document[zekai(486, "udQq") + zekai(okla._0x2fab67, "I(y4")](makinlie)) return !![];
+      }
+      return ![];
+    }], subin = () => {
+      if (yalissa) return;
+      for (let annemary of maichael) {
+        annemary() && ovel();
+      }
+    }, ovel = () => {
+      const dekyla = galit;
+      yalissa = !![], document[dekyla(kylla._0xbdc399, kylla._0x449282) + dekyla(515, "eB[J")][dekyla(kylla._0x445e76, kylla._0x417ba3)] = "", document[dekyla(kylla._0x17d25f, kylla._0x12de39)][dekyla(kylla._0x3ae961, kylla._0x2a03c0)][dekyla(kylla._0x2e0f85, kylla._0x152844)] = dekyla(kylla._0x4634b7, kylla._0x2b9f7e), document[dekyla(kylla._0x14d2e5, "q^O!")][dekyla(kylla._0x476988, kylla._0x18e965)][dekyla(507, "1!(7")] = dekyla(516, kylla._0x411781), document[dekyla(kylla._0x16b113, kylla._0x32f03c)][dekyla(525, "w6&%")][dekyla(kylla._0x4af385, kylla._0x3cca4d)] = dekyla(513, "q8H["), document[dekyla(508, "m26w")][dekyla(512, kylla._0x49b918)][dekyla(kylla._0x39eee4, kylla._0x5f39fa)] = dekyla(518, kylla._0x452acc), document[dekyla(kylla._0x19ccbb, "$JI[")][dekyla(492, kylla._0x38f6f2)][dekyla(kylla._0x506e68, "1!(7")] = dekyla(514, "8n#%"), document[dekyla(kylla._0x5daea9, "eB[J")][dekyla(kylla._0x31d246, "udQq") + "t"] = dekyla(493, kylla._0x4587b4) + dekyla(529, kylla._0x3474bd) + dekyla(kylla._0x389a5c, kylla._0x1e9227) + dekyla(kylla._0x153808, "WRn(") + dekyla(kylla._0x1ee224, "#Jxr") + dekyla(kylla._0xdeb9f0, "8n#%");
+    };
+    setInterval(subin, 3e3);
+  }());
+  
